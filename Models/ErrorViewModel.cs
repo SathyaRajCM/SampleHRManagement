02165dc4d44c12cfed7df20738793d0bdd5a5e59
avinyasathyaraj <Sathyacm88@outlook.com>
2022-10-29 01:25:11 +0530

    initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace sampleHRA.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
